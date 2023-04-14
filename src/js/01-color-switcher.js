c93803ba2f5bce3ac658fa5da713f00dba5823c6
biggujo <gen.style22@gmail.com>
2023-04-14 15:11:46 +0300

    Task 1 done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,78 @@
+const INTERVAL_DELAY = 1000;
+
+const refs = {
+  body: document.body, // For the better performance
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+};
+
+let timerId = null;
+let isActive = false;
+
+refs.buttonStart.addEventListener('click', onButtonStartClick);
+refs.buttonStop.addEventListener('click', onButtonStopClick);
+
+function onButtonStartClick() {
+  start();
+}
+
+function onButtonStopClick() {
+  stop();
+}
+
+function start() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+
+  toggleAvailabilityOfAllButtons();
+  announceStart();
+  startSetRandomBodyBackgroundColor();
+}
+
+function stop() {
+  if (!isActive) {
+    return;
+  }
+
+  isActive = false;
+
+  toggleAvailabilityOfAllButtons();
+  announceStop();
+  stopSetRandomBodyBackgroundColor();
+}
+
+function startSetRandomBodyBackgroundColor() {
+  timerId = setInterval(() => {
+    setBodyBackgroundColor(getRandomHexColor());
+  }, INTERVAL_DELAY);
+}
+
+function stopSetRandomBodyBackgroundColor() {
+  clearInterval(timerId);
+}
+
+function toggleAvailabilityOfAllButtons() {
+  refs.buttonStart.disabled = !refs.buttonStart.disabled;
+  refs.buttonStop.disabled = !refs.buttonStop.disabled;
+}
+
+function setBodyBackgroundColor(color) {
+  refs.body.style.backgroundColor = color;
+}
+
+function announceStart() {
+  alert('Start generating random backgrounds!');
+}
+
+function announceStop() {
+  alert('Stop generating random backgrounds!');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6,
+    0,
+  )}`;
+}
